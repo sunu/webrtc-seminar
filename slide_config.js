308535473fc5b0facc5a2e0cd64412ba7da97234
Tarashish Mishra <sunu0000@gmail.com>
2014-01-15 23:38:34 +0530

    My webrtc seminar stuff.

---
+++ slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'WebRTC',
+    subtitle: 'Pluginless Realtime Communication for the web',
+    eventInfo: {
+     title: 'Seminar',
+     date: '16/1/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Tarashish Mishra',
+    company: '6th semester, CSE',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@tarashish',
+    www: 'http://www.sunu.in',
+    github: 'http://github.com/sunu'
+  }]
+};
+
